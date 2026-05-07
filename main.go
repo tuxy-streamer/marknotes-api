7f00e02ffd19c25262b6b897c93c3e6be06b237f
tuxy-streamer <subham020304@gmail.com>
2026-05-08 00:02:07 +0530

    feat: add list-notes endpoint

--- main.go
+++ main.go
@@ -48,7 +48,45 @@ func saveNotesHandler(rw http.ResponseWriter, req *http.Request) {
 	rw.Write([]byte(msg))
 }
 
-func listNotes(rw http.ResponseWriter, req *http.Request) {}
+func getNotesList() (map[string][]string, error) {
+	entries, err := os.ReadDir("./")
+	if err != nil {
+		return nil, err
+	}
+	noteList := make(map[string][]string, 0)
+	for _, e := range entries {
+		if e.IsDir() && e.Name() != ".git" {
+			file, err := os.ReadDir(e.Name())
+			if err != nil {
+				return nil, err
+			}
+			tempFileName := make([]string, 0)
+			for _, f := range file {
+				if !f.IsDir() {
+					tempFileName = append(tempFileName, f.Name())
+				}
+			}
+			noteList[e.Name()] = tempFileName
+		}
+	}
+	return noteList, nil
+}
+
+func listNotesHandler(rw http.ResponseWriter, req *http.Request) {
+	noteList, err := getNotesList()
+	if err != nil {
+		http.Error(rw, "Failed to get notes list", http.StatusInternalServerError)
+		return
+	}
+	payload, err := json.Marshal(noteList)
+	if err != nil {
+		http.Error(rw, "Error: Failed to generate json payload", http.StatusInternalServerError)
+		return
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(payload)
+}
 
 func checkGrammar(rw http.ResponseWriter, req *http.Request) {}
 
@@ -77,7 +115,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/save-notes", saveNotesHandler)
-	// mux.HandleFunc("/list-notes", listNotes)
+	mux.HandleFunc("/list-notes", listNotesHandler)
 	// mux.HandleFunc("/check-grammar", checkGrammar)
 	// mux.HandleFunc("/parse-html", parseHtml)
 	http.ListenAndServe(":3000", mux)
